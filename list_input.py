965cdfb493e55c7cb847e3fff775d4fdbeeaca10
Milen <milengminev@gmail.com>
2014-03-18 15:19:31 +0200

    fixed an error in Create method

--- list_input.py
+++ list_input.py
@@ -7,11 +7,12 @@ class ListInput():
 
     def create(self, name):
         self.list_name = name
-        if self.list_name in all_lists:
+        if self.list_name in self.all_lists:
             print ('A list with name ' + self.list_name + ' already exists!') # if name of list is taken
         else:
             print ('new list ' + self.list_name + ' was created.') # prints message if create is successful
             self.all_lists.append(self.list_name)
+            print (self.all_lists)
             return self.new_list
 
     def add(self):
--- list_input_tests.py
+++ list_input_tests.py
@@ -3,10 +3,13 @@ import unittest
 
 
 class ListInputTests(unittest.TestCase):
-    def test_create(self):
+    def setUp(self):
         self.Mylist = ListInput('People')
-        #self.assertEqual(('new list People was created.'), ListInput.create('People'))
+        self.Mylist.create('first')
+    def test_create(self):
         self.assertEqual([], self.Mylist.create('name_of_list'))
+    def test_create_when_name_is_taken(self):
+        self.assertEqual(None, self.Mylist.create('first'))
 
 
 if __name__ == '__main__':
