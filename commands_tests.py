46726b21945cadbccba818d3ede9aad5e771203c
Emilian <emilian@emilian-ThinkPad-T61.(none)>
2014-03-20 13:49:57 +0200

    Added create list command

--- commands_tests.py
+++ commands_tests.py
@@ -5,6 +5,12 @@ import unittest
 class TestCommands(unittest.TestCase):
     def test_create_list(self):
         self.assertEqual('Test_List', commands.create_list('Test_List'))
+        self.assertEqual('Test_List 2', commands.create_list('Test_List 2'))
+
+    #def test_add_user(self):
+     #   self.assertEqual('Test_List', commands.create_list('Test_List'))
+     #   self.assertEqual('Test_List 2', commands.create_list('Test_List 2'))
+     #   self.assertEqual('[1] User - email@mail.com',commands.add(1))
 
 
 if __name__ == '__main__':
