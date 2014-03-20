ba2a65e28b79e43f4a02d869fffb86d16a3f292d
Emilian <emilian@emilian-ThinkPad-T61.(none)>
2014-03-20 13:49:09 +0200

    Added create list command

--- commands.py
+++ commands.py
@@ -2,14 +2,28 @@ from user import User
 from list import List
 
 
+index = 1
+users_index = 1
+lists = []
+
 def create_list(name):
+    global index
+    global lists
     mailing_list = List(name)
     file = open(mailing_list.name, 'w')
     file.close()
+    file = open('mailing_lists.txt', 'a')
+    file.write('[' + str(index) + '] ' + mailing_list.name + '\n')
+    file.close()
+    lists.append(mailing_list.name)
+    index += 1
     return mailing_list.name
 
 
-#def add(identifier):
-#   file = open(lists[identifier], 'w')
-#    name = input('name >>>')
-#    file.write(name)
+def add(identifier):
+    file = open(lists[identifier - 1], 'a')
+    name = input('name >>>')
+    file.write('[' + users_index + '] ' + name + ' - ')
+    email = input('email >>>')
+    file.write(email + '\n')
+    file.close()
